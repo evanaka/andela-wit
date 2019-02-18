fe8d6673f322a1dacabc957b25dc1dfbcaa06943
evanaka <atuhaireeva123@gmail.com>
2019-02-18 10:43:41 +0300

    dictionary challenge 1

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+#creating a dictionary for keys and integers and there squares as values.
+numbers = {
+    "1": 1**2,
+    "2": 2**2,
+    "3": 3**2,
+    "4": 4**2,
+    "5": 5**2,
+    "6": 6**2,
+    "7": 7**2,
+    "8": 8**2,
+    "9": 9**2,
+    "10": 10**2,
+    "11": 11**2,
+    "12": 12**2,
+    "13": 13**2,
+    "14": 14**2,
+    "15": 15**2,
+}
+for key, value in numbers . items ():
+    print("The square of: "  + str(key) +" is: " + str (value))
